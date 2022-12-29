c8ccfa9979fd2a405bcd7d1202f8a15adb8e9a6d
Guillermo Torres <98137107+Guilleto98@users.noreply.github.com>
2022-12-29 16:01:15 -0300

    Test Customize

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
-import { Box } from '@chakra-ui/react'
+import {
+  Box,
+  Button} from '@chakra-ui/react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Box bg='brand.primary'>
+      <Box bg='brand.accent' d>
           Hola Mundo
       </Box>
+      <Button size='lg' variant='cta'>This is</Button>
+      <Button size='lg' variant='ghost'>This is</Button>
+      <Button size='lg' variant='outline'>This is</Button>
     </div>
   );
 }
--- src/resources/theme/index.js
+++ src/resources/theme/index.js
@@ -2,13 +2,35 @@
 import { extendTheme } from "@chakra-ui/react"
 
 // 2. Call `extendTheme` and pass your custom values
-export const theme = extendTheme({
-  colors: {
+const colors = {
     brand: {
       primary: "#f7fafc",
       secondary: "#1a202c",
       accent: "#69306D"
     },
-  },
-})
+  }
 
+const Button = {
+    baseStyle:{
+        fontWeight:'light',
+        _hover:{
+            boxShadow: "rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px"
+        }
+    },
+    sizes:{
+        lg:{
+            fontWeight:'bold',
+            borderRadius: '10px'
+        }
+    },
+    variants:{
+        outline:{
+            _hover:{
+                bg: 'brand.accent',
+                color:'white'
+            }
+        }
+    }
+}
+
+export const theme = extendTheme({colors, components: {Button}})
